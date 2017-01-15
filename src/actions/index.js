9c5d5d60c94ff9b99f5b09695d1c2af3fd6bfc95
ken5scal <kengoscal@gmail.com>
2017-01-15 15:00:51 +0900

    Bind ActionCreator to Container

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,9 @@
 export function selectBook(book) {
-  console.log("A book has been selected", book.title);
+  // console.log("A book has been selected", book.title);
+  // select Book is an ActionCreator, it needs to return an acton,
+  // an object with a type property
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
