72c505cea6a89c8194f8c240c71179fa153cf800
ken5scal <kengoscal@gmail.com>
2017-01-15 16:17:25 +0900

    Finish first redux-react connection

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,5 @@
 export function selectBook(book) {
-  // console.log("A book has been selected", book.title);
-  // select Book is an ActionCreator, it needs to return an acton,
+  // selectBook is an ActionCreator, it needs to return an acton,
   // an object with a type property
   return {
     type: 'BOOK_SELECTED',
